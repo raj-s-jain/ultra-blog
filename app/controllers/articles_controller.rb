bef98a90da7f5557003fb40296ced39106ee6c0a
trumper <raj.s.jain@hotmail.com>
2016-02-09 16:47:37 +0530

    replaced

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,25 +3,28 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-
+  
   def new
-    @article =  Article.new
+    @article = Article.new
   end
-
+  
   def edit
+    
   end
-
+  
   def update
+   
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:flash] = "Article was successfully updated!"
       redirect_to article_path(@article)
     else
-      render 'update'
+      render 'edit'
     end
   end
-
+  
   def create
     @article = Article.new(article_params)
+    
     if @article.save
       flash[:notice] = "Article was successfully created."
       redirect_to article_path(@article)
@@ -29,26 +32,25 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-
-  #comment
-
+  
   def show
+    
   end
-
+  
   def destroy
+    
     @article.destroy
-    flash[:notice] = "Article Deleted"
+    flash[:notice] = "Article was successfully deleted."
     redirect_to articles_path
   end
-
+  
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    def article_params
-      params.require(:article).permit(:title, :description)
-    
-    end
-
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  
 end
