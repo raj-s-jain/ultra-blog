aa8b9964d73034385e1ea7159c9e85ee3a85fd68
trumper <raj.s.jain@hotmail.com>
2016-02-16 00:41:46 +0530

    Adding users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, 
               uniqueness: { case_sensitive: false}, 
               length: { minimum: 3, maximum: 25}
-  VALID_EMAIL_REGEX = /\A{\w+\-,]+@[a-z\d\-,]+\.[a-z]+\z/i  
+VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
               uniqueness: { case_sensitive: false },
               length: { maximum: 105},
